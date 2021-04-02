438adada7f4d722bd54041e2a69242bcbff6ebe9
Tony Lewis <tonyelewis@users.noreply.github.com>
2021-04-02 09:04:05 +0100

    Update extract-cmake-flags.py code

--- extract-cmake-flags.py
+++ extract-cmake-flags.py
@@ -1,108 +1,179 @@
 #!/usr/bin/env python3
 
-from subprocess import PIPE, run
-from tempfile   import TemporaryDirectory
-from pathlib    import Path
-from typing     import List
+import itertools
+import logging
+import shlex
+import subprocess
+import tempfile
+
+from pathlib import Path
+from typing import Dict, List, Optional, Set
 
 import argparse
 import json
 import sys
 
-all_args = sys.argv[ 1: ]
-num_arg_breakers = all_args.count( '--' )
-if num_arg_breakers > 1:
-	raise Exception( "The command line arguments breaker '--' should only be used at most once" )
+# Example commands:
+#   extract-cmake-flags.py                                                                    -- -DCMAKE_TOOLCHAIN_FILE=$(ls -1d ~/puppet/toolchain-files/clang_rwdi.cmake )
+#   extract-cmake-flags.py --conan-profile-file $(ls -1d ~/puppet/conan-profiles/clang_rwdi ) -- -DCMAKE_TOOLCHAIN_FILE=$(ls -1d ~/puppet/toolchain-files/clang_rwdi.cmake )
 
-arg_breaker_index = all_args.index( '--' ) if ( num_arg_breakers == 1 ) else len( all_args )
-local_args        = all_args[ 0                     : arg_breaker_index ]
-cmake_args        = all_args[ arg_breaker_index + 1 : len( all_args )   ]
-
-arg_parser = argparse.ArgumentParser(
-	description="Extract the -std=..., -D... and -isystem flags from the compile commands CMake generates.",
-	epilog="Append arguments to pass-through to CMake after a dummy '--' argument",
-)
-arg_parser.add_argument('--conan-profile-file', type=Path,
-                   help='Run Conan before CMake using the specified file')
-
-args = arg_parser.parse_args( local_args )
-
-print( f'Conan profile file                 : ' + str( args.conan_profile_file ), file=sys.stderr )
-print( f'Arguments to pass through to CMake : { " ".join( cmake_args ) }', file=sys.stderr )
-
-temp_dir_handle    = TemporaryDirectory()
-temp_dir           = Path( temp_dir_handle.name )
-
-if args.conan_profile_file is not None:
-	# Run conan
-	conan_command: List[str] = [
-		'conan',
-		'install',
-		'--build', 'missing',
-		'--install-folder', str( temp_dir ),
-		'.',
-		'--profile', str( args.conan_profile_file ),
-	]
-	print( "About to run Conan command:\n\t" + ( ' ' . join( conan_command ) ), file=sys.stderr )
-	conan_result = run( conan_command, stdout=PIPE, stderr=PIPE )
-	print( "\nConan stdout:\n" + conan_result.stdout.decode() + "\nConan stderr:\n" + conan_result.stderr.decode(), file=sys.stderr )
-	conan_result.check_returncode()
-
-cmake_command: List[str] = [
-	'cmake',
-	( '-B' + str( temp_dir ) ),
-	'-H.',
-	'-DCMAKE_EXPORT_COMPILE_COMMANDS=ON',
-	*cmake_args
+# A list of standard build directories, in which to search for include directories that are constructed as part of the build (eg git version headers)
+CANDIDATE_REL_INC_BASES: List[Path] = [
+	Path('ninja_clang_dbgchk'),
+	Path('ninja_clang_debug'),
+	Path('ninja_clang_memsan'),
+	Path('ninja_clang_rwdi'),
+	Path('ninja_clang_thrsan'),
+	Path('ninja_clang_ubasan'),
+	Path('ninja_gcc_dbgchk'),
+	Path('ninja_gcc_debug'),
+	Path('ninja_gcc_rwdi'),
+	Path('ninja_gcc_ubasan'),
 ]
-print( "About to run CMake command:\n\t" + ( ' ' . join( cmake_command ) ), file=sys.stderr )
-cmake_result = run( cmake_command, stdout=PIPE, stderr=PIPE  )
-print( "\nCMake stdout:\n" + cmake_result.stdout.decode() + "\nCMake stderr:\n" + cmake_result.stderr.decode(), file=sys.stderr )
-cmake_result.check_returncode()
 
-# Read the compile commands data
-compile_commands_file: Path = temp_dir / 'compile_commands.json'
-with open( compile_commands_file, 'r') as compile_commands_fh:
-	compile_commands = json.loads( compile_commands_fh.read() )
+logging.basicConfig(
+	format='%(asctime)s [ %(levelname)s ] %(message)s',
+	level=logging.INFO,
+	stream=sys.stderr,
+)
+logger = logging.getLogger( __name__ )
 
-def extract_flags_from_cmake_db(compile_commands):
-	definitions     = {}
-	system_includes = set()
-	local_includes  = set()
-	cpp_standard    = None
-	for compile_command in compile_commands:
-		command_parts = compile_command[ 'command' ].split()
-		for [ cmd_idx, cmd_part ] in enumerate( command_parts ):
+def resolve_include_dir(query_dir: Path,
+                        *,
+                        candidate_rel_inc_bases: List[Path]
+                        ) -> Path:
+	'''
+	Find the an existing directory that matches the specified include directory, relative to one of the base directories
+	(which are typically build directories) or just return the original if none can be found
+
+	This is used to handle finding an include directory that's constructed as part of the build (eg git version headers)
+
+	:param query_dir               : The relative dir to search for (eg Path('source/external_info))
+	:param candidate_rel_inc_bases : A list of build directories in which special, build-constructed include directories might be found (eg [ Path('ninja_clang_dbgchk'), ...])
+	'''
+	if not query_dir.is_absolute():
+		for candidate_rel_inc_base in candidate_rel_inc_bases:
+			if (candidate_rel_inc_base / str(query_dir)).is_dir():
+				return (candidate_rel_inc_base / str(query_dir)).resolve()
+	return query_dir
+
+
+def extract_flags_from_cmake_db(*,
+                                cmake_commands_db: List[dict],
+                                candidate_rel_inc_bases: List[Path]
+                                ) -> List[str]:
+	'''
+	Extract the union of all the flags of interest in all the commands
+
+	:param cmake_commands_db       : The commands
+	:param candidate_rel_inc_bases : A list of build directories in which special, build-constructed include directories might be found
+	'''
+	definition_of_macro_name: Dict[str,str] = {}
+	system_includes: Set[Path] = set()
+	local_includes: Set[Path] = set()
+	cpp_standard: Optional[str] = None
+
+	# Grab all the flags of interest from all the commands
+	for compile_command in cmake_commands_db:
+		command_parts = compile_command['command'].split()
+		for [cmd_idx, cmd_part] in enumerate(command_parts):
 			if cmd_part == '-isystem':
-				system_includes.add( command_parts[ cmd_idx + 1 ] )
-			if cmd_part.startswith( '-I' ):
+				system_includes.add(Path(command_parts[cmd_idx + 1]))
+			if cmd_part.startswith('-I'):
 				if cmd_part == '-I':
-					local_includes.add( command_parts[ cmd_idx + 1 ] )
+					local_includes.add(Path(command_parts[cmd_idx + 1]))
 				else:
-					local_includes.add( cmd_part[2:] )
-			elif cmd_part.startswith( '-D' ):
-				definition_key = cmd_part[2:].split( '=' )[ 0 ]
-				definitions[ definition_key ] = cmd_part
-			elif cmd_part.startswith( '-std=' ):
+					local_includes.add(Path(cmd_part[2:]))
+			elif cmd_part.startswith('-D'):
+				definition_key = cmd_part[2:].split('=')[0]
+				definition_of_macro_name[definition_key] = cmd_part
+			elif cmd_part.startswith('-std='):
 				cpp_standard = cmd_part[5:]
+
+	# Return all the parts
 	return [
-		*sorted( definitions.values() ),
-		*list( map(
-			lambda x: '-I' + x,
-			sorted( local_includes )
-		) ),
-		*list( map(
-			lambda x: '-isystem ' + x,
-			sorted( system_includes )
-		) ),
-		*( [] if cpp_standard is None else [ f'-std={cpp_standard}' ] )
+		*sorted(definition_of_macro_name.values()),
+		*[
+			'-I' + str(resolve_include_dir(x, candidate_rel_inc_bases=candidate_rel_inc_bases))
+			for x in sorted(local_includes)
+		],
+		*itertools.chain.from_iterable(
+			('-isystem', str(resolve_include_dir(x, candidate_rel_inc_bases=candidate_rel_inc_bases)))
+			for x in sorted(system_includes)
+		),
+		*([] if cpp_standard is None else [f'-std={cpp_standard}'])
 	]
 
-flags = extract_flags_from_cmake_db(compile_commands)
 
-# print(
-# 	"export CMAKE_EXTRACTED_COMPILE_FLAGS=' "
-# 		+ ' '.join( sorted( flags ) )
-# 		+ " '"
-# )
-print( ' '.join( sorted( flags ) ) )
+def main():
+	'''
+	The main function, to prevent global variables
+	'''
+
+	# Prepare an ArgumentParser
+	arg_parser = argparse.ArgumentParser(
+		description="Extract the -std=..., -D... and -isystem flags from the compile commands CMake generates.",
+		epilog="Append arguments to pass-through to CMake after a dummy '--' argument",
+	)
+	arg_parser.add_argument('--conan-profile-file', type=Path,
+                         help='Run Conan before CMake using the specified file')
+
+	# Process the command-line arguments
+	all_args = sys.argv[1:]
+	num_arg_breakers = all_args.count('--')
+	if num_arg_breakers > 1:
+		arg_parser.error("The command line arguments breaker '--' should only be used at most once")
+	arg_breaker_index = all_args.index('--') if (num_arg_breakers == 1) else len(all_args)
+	local_args = all_args[0: arg_breaker_index]
+	cmake_args = all_args[arg_breaker_index + 1: len(all_args)]
+	args = arg_parser.parse_args(local_args)
+
+	# Log the args
+	logger.info(f'Conan profile file                 : { str(args.conan_profile_file) }')
+	logger.info(f'Arguments to pass through to CMake : { " ".join( cmake_args ) }')
+
+	# In a temporary directory...
+	with tempfile.TemporaryDirectory() as temp_dir_handle:
+		temp_dir = Path(temp_dir_handle)
+
+		# If there's a Conan profile command, run Conan
+		if args.conan_profile_file is not None:
+			conan_command: List[str] = [
+				'conan',
+				'install',
+				'--build', 'missing',
+				'--install-folder', str(temp_dir),
+				'.',
+				'--profile', str(args.conan_profile_file),
+			]
+			logger.info(f'About to run Conan command: { shlex.join(conan_command) }')
+			conan_result = subprocess.run(conan_command, check=False, capture_output=True, text=True)
+			print(f"\nConan stdout:\n{ conan_result.stdout }\nConan stderr:\n{ conan_result.stderr }", file=sys.stderr)
+			conan_result.check_returncode()
+
+		# Run CMake
+		cmake_command: List[str] = [
+			'cmake',
+			('-B' + str(temp_dir)),
+			'-H.',
+			'-DCMAKE_EXPORT_COMPILE_COMMANDS=ON',
+			*cmake_args
+		]
+		logger.info(f'About to run CMake command: { shlex.join(cmake_command) }')
+		cmake_result = subprocess.run(cmake_command, check=False, capture_output=True, text=True)
+		print(f"\nCMake stdout:\n{ cmake_result.stdout }\nCMake stderr:\n{ cmake_result.stderr }", file=sys.stderr)
+		cmake_result.check_returncode()
+
+		# Read the compile commands data
+		with open(temp_dir / 'compile_commands.json', 'r') as compile_commands_fh:
+			flags = extract_flags_from_cmake_db(
+				cmake_commands_db=json.loads(compile_commands_fh.read()),
+				candidate_rel_inc_bases=CANDIDATE_REL_INC_BASES,
+			)
+
+	# Print the extracted flags
+	print(shlex.join(flags))
+
+
+if __name__ == "__main__":
+	main()
